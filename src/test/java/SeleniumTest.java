e240799cece1ddde41cc38e79293a52c097bd2fe
D_Krasik <dawid.krasik@capgemini.com>
2022-07-13 15:49:29 +0200

    WorkSpcace Selenium intro

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,15 +1,37 @@
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
 
-    }
 
     @Test
-    public void sampleTest(){
-        System.out.println("Hello!!");
+    public void openFirefoxGoodlePage(){
+        //System.setProperty("webdriver.gecko.driver","D:\\Java_Projects\\Udemy_Selenium\\Udemy_Selenium\\src\\main\\resources\\firefox\\geckodriver.exe");
+
+
+        WebDriver driver = getDriver("chrome");
+        driver.manage().window().maximize();
+        //System.out.println("Hello!!");
+        driver.get("https://www.google.pl/");
+    }
+    @Test
+    public void openChromeGoodlePage(){
+        System.setProperty("webdriver.chrome.driver","D:\\Java_Projects\\Udemy_Selenium\\Udemy_Selenium\\src\\main\\resources\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //System.out.println("Hello!!");
+        driver.get("https://www.google.pl/");
+    }
+    public WebDriver getDriver(String browser){
+        switch (browser){
+            case "chrome":
+                String chromePath = "D:\\Java_Projects\\Udemy_Selenium\\Udemy_Selenium\\src\\main\\resources\\chrome\\chromedriver.exe";
+                System.setProperty("webdriver.chrome.driver",chromePath);
+                return  new ChromeDriver();
+            default:
+                throw  new InvalidArgumentException("Wrong Browser ... try Difrend one ");
+        }
     }
 }
